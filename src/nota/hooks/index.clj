d649f57018d67d70cb7142a7e8caef1d05916edd
Rafael Delboni <rafadelboni@gmail.com>
2021-11-09 19:47:35 -0300

    fix: Site responsiviness and viewport

--- src/nota/hooks/index.clj
+++ src/nota/hooks/index.clj
@@ -26,6 +26,8 @@
     [:head
      [:title title]
      [:meta {:charset "utf-8"}]
+     [:meta {:http-equiv "x-ua-compatible" :content "ie=edge"}]
+     [:meta {:name "viewport" :content "width=device-width, initial-scale=1, maximum-scale=1"}]
      (for [{:keys [href rel htype]} links]
        [:link {:rel rel
                :type htype
